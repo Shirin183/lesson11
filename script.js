eb64ffb07b85b90877eb691ef638710650d73ab0
Shirin183 <rumayarumiya@gmail.com>
2025-04-17 11:34:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let box = document.querySelector(".box");
+let bgcolor = prompt("enter body backgroundColor");
+
+let color = prompt("enter text color");
+
+
+document.body.style.backgroundColor = bgcolor ;
+document.body.style.color = color ;
